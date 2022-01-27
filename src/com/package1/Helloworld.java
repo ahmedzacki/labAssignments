f5fdd268e1e044fbac4fc1f6236a238b9e898680
ahmed <ahmedxandulle01@gmail.com>
2022-01-26 21:50:10 -0500

    Initial commit

---
+++ src/com/package1/Helloworld.java
@@ -0,0 +1,9 @@
+package com.package1;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to My Software Dev 1 Course");
+    }
+}
